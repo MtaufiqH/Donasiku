54831963fc2f90d3aacafa35be54bef45049c476
M. Taufiq Hidayat <mtaufiqh62@gmail.com>
2021-01-21 21:16:40 +0800

    complete detail

--- app/src/main/java/id/taufiq/donasiku/views/DetailActivity.kt
+++ app/src/main/java/id/taufiq/donasiku/views/DetailActivity.kt
@@ -1,10 +1,13 @@
 package id.taufiq.donasiku.views
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import id.taufiq.donasiku.R
 import id.taufiq.donasiku.data.response.DonasiItem
-import kotlinx.android.synthetic.main.donasi_list.*
+import id.taufiq.donasiku.utils.formatRupiahs
+import kotlinx.android.synthetic.main.activity_detail.*
 
 class DetailActivity : AppCompatActivity() {
 
@@ -13,10 +16,19 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        intent.getParcelableExtra<DonasiItem>("DATA")?.apply {
+            tv_title_detail.text = this.title
+            tv_current_detail.text = formatRupiahs(this.currentDonation.toDouble())
+            tv_target_detail.text = formatRupiahs(this.targetDonation.toDouble())
+            progressBar_detail.max = this.targetDonation.toInt()
+            progressBar_detail.progress = this.currentDonation.toInt()
+            tv_cerita_detail.text = this.description
+            Glide.with(this@DetailActivity).load(this.photo).into(iv_poster_detail)
+        }
 
-        val data = intent.getParcelableExtra<DonasiItem>("DATA")
-        if (data != null) {
-
+        btn_donasi.setOnClickListener {
+            Toast.makeText(this, "terima kasih", Toast.LENGTH_SHORT).show()
         }
 
 
